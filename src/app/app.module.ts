d43c23fc517b9bd586295548ba7aca2f2419f114
Pablo Sanchez <soportepablos@gmail.com>
2022-01-30 16:43:31 -0300

    Inicio html navmenu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavmenuComponent } from './components/navmenu/navmenu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavmenuComponent
   ],
   imports: [
     BrowserModule
